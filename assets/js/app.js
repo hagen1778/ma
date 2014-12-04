2f4004ac2cfe17d257f9a3e4d6bb64c029d7b4cc
hagen1778 <hagen1778@gmail.com>
2014-12-04 18:50:14 +0200

    home page

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,19 +10,25 @@
                     return 'page/' + params.id + '.html';
                 }
             })
+            .when('/', {
+                controller: 'PageController',
+                templateUrl: 'page/home.html'
+            }
+            )
             .otherwise({redirectTo: '/'});
     });
 
     app.controller('PageController', function ($scope, $location) {
-  /*      $scope.tabs = [
-         {id: 'dummyTable', title: 'Dummy Table', order: 1, path: 'tabs/works.html'},
-         {id: 'dummyChart', title: 'Dummy Chart', order: 2, path: 'tabs/cv.html'},
-         {id: 'dummyList', title: 'Dummy List', order: 0, path: 'tabs/contact.html'}
-         ];*/
+        $scope.tabs = [
+            {title: 'ma', path: ''},
+            {title: 'works', path: 'works'},
+            {title: 'cv', path: 'cv'},
+            {title: 'contact', path: 'contact'}
+        ];
 
-         $scope.isActive = function (route) {
-         return route === $location.path();
-         }
+        $scope.isActive = function (route) {
+            return route === $location.path();
+        }
     });
 
 })();
